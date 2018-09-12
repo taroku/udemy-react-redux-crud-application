18d74e46f09b1ded049867a9b52bcc20e804d902
taroku <suteadosu@gmail.com>
2018-09-13 02:45:30 +0900

    fix typo for action export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
-import reducer from './reducers'
+import reducer from './reducers';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,4 +1,4 @@
-import { INCREMENT,DECREMENT } from '../actions'
+import { INCREMENT, DECREMENT } from '../actions';
 
 // 状態の初期値をオブジェクトとして定義する。
 const initialState = { value: 0 }
@@ -9,7 +9,7 @@ export default (state = initialState,action) => {
       return {value: state.value +1}
     case DECREMENT:
       return {value: state.value -1}
-    default
+    default:
       return state
   }
 }
