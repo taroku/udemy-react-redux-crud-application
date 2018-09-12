7ebf37657ae95f83dc4262a1113a068337b078b6
taroku <suteadosu@gmail.com>
2018-09-12 17:13:21 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 //ReactはJSXを使うときはスコープに存在しないといけない
 //JSXのタグはtranspileすると、JSに変換される。Reactがtranspileを担当する。
-class App extends Component {
-  render() {
-    // const greeting = "Hi!TOM!"
-    // const dom = <h1 className="foo">{greeting}</h1>
-    // return  dom
-    // return <input type="text" onClick={ () => {console.log("I am clicked")}}/>
-    // return <input type="text" onChange={ () => {console.log("I am clicked")}}/>
-    return(
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={ () => {console.log("I am clicked")}}/>
-      </React.Fragment>
-    )
-  }
-}
+// class App extends Component {
+//   render() {
+//     // const greeting = "Hi!TOM!"
+//     // const dom = <h1 className="foo">{greeting}</h1>
+//     // return  dom
+//     // return <input type="text" onClick={ () => {console.log("I am clicked")}}/>
+//     // return <input type="text" onChange={ () => {console.log("I am clicked")}}/>
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={ () => {console.log("I am clicked")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
 
 // class App extends Component {
 //   render() {
@@ -27,5 +27,15 @@ class App extends Component {
 //   }
 // }
 
+const App = () =>{
+  return <React.Fragment>
+    <Cat/>
+    <Cat/>
+    <Cat/>
+  </React.Fragment>
+}
+const Cat = ()=>{
+  return <h1>Meow!</h1>
+}
 
 export default App;
