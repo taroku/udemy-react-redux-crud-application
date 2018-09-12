c248f9c1b21b7ad337f64e98350e6244ceecc9e0
taroku <suteadosu@gmail.com>
2018-09-12 22:51:25 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-//ReactはJSXを使うときはスコープに存在しないといけない
-//JSXのタグはtranspileすると、JSに変換される。Reactがtranspileを担当する。
-class App extends Component {
-  render() {
-    // const greeting = "Hi!TOM!"
-    // const dom = <h1 className="foo">{greeting}</h1>
-    // return  dom
-    // return <input type="text" onClick={ () => {console.log("I am clicked")}}/>
-    // return <input type="text" onChange={ () => {console.log("I am clicked")}}/>
-    return(
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={ () => {console.log("I am clicked")}}/>
-      </React.Fragment>
-    )
-  }
+import React from 'react';
+import PTypes from 'prop-types';
+
+const App = () =>{
+  const profiles = [
+    { name:"Taro",age:10},
+    { name:"Hanako",age:5},
+    { name:"Rin"}
+  ]
+  return (
+  <React.Fragment>
+    {
+      profiles.map((profile,index)=>{
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })
+    }
+    {/* <User name = {"Taro"} age={10}/>
+    <User name = {"Hanako"} age={5}/> */}
+  </React.Fragment>
+  )
+}
+const User = (props)=>{
+  return <h1>Hi!I am {props.name},and {props.age}years old!</h1>
+}
+User.defaultProps = {
+  age:5
 }
 
-// class App extends Component {
-//   render() {
-//     return  React.createElement(
-//       "h1",
-//       null,
-//       "Hello,world!!"
-//     );
-//   }
-// }
-
+User.propTypes = {
+  name: PTypes.string,
+  age:PTypes.number.isRequired
+}
 
 export default App;
